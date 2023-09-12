c160eb574389b24e46794c536bdd62c6a1ece3d1
Maksudr <maksudurrahamanmishu7@gmail.com>
2023-09-12 13:33:31 +0600

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './ComponentHeader/Header'
+import Blocks from './blocks/Blocks'
+
+function App() {
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Blocks></Blocks>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/blocks/Blocks.jsx
@@ -0,0 +1,11 @@
+
+
+const Blocks = () => {
+    return (
+        <div>
+            <h1>jjjj</h1>
+        </div>
+    );
+};
+
+export default Blocks;
